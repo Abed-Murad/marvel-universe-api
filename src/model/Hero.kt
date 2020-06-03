9b340660998c4b9771e1fafc7787c9df95f009ce
Abed Murad <Abdallah.Murad@ProtonMail.com>
2020-06-03 19:25:45 +0300

    Update Hero Model + Create Heroes Table.

--- src/model/Hero.kt
+++ src/model/Hero.kt
@@ -1,19 +1,32 @@
 package marvel_universe_api.model
 
+import org.jetbrains.exposed.sql.Table
+
+object Heroes : Table() {
+    val id = integer("id")
+    val name = varchar("name", 255)
+    val quote = varchar("quote", 255)
+    val color = varchar("color", 255)
+    val poster = varchar("poster", 255)
+    val dateUpdated = long("dateUpdated")
+    override val primaryKey = PrimaryKey(id)
+}
+
+
 data class Hero(
-        val id: Int,
-        val name: String,
-        val quote: String,
-        val color: String,
-        val poster: String,
-        val details: List<Detail>
+    val id: Int,
+    val name: String,
+    val quote: String,
+    val color: String,
+    val poster: String,
+    val dateUpdated: Long
+
 )
 
-data class Detail(
-        val id: Int,
-        val name: String,
-        val plot: String,
-        val poster: String,
-        val rootId: Int
+data class NewHero(
+    val id: Int?,
+    val name: String,
+    val quote: Int,
+    val color: String,
+    val poster: String
 )
-
